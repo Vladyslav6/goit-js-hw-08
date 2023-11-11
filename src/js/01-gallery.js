20665c93bb0c8ef64b5c9bbb399eefb642dcacbf
Vladyslav6 <vlad51284@gmail.com>
2023-11-11 02:14:27 +0200

    next com

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const ulGallery = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+ulGallery.insertAdjacentHTML('beforeend', itemsMarkup);
+
+// rendered items
+function createGalleryItemsMarkup(items) {
+  return items
+
+    .map(({ preview, original, description }) => {
+      return `<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
